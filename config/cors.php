9f844f903fbd45f7593c9a62901fe0178b1795f3
pedroRH10 <pedrorobertohi@gmail.com>
2026-05-22 01:35:27 +0200

    CORS: aceptar cualquier subdominio azurestaticapps.net

--- config/cors.php
+++ config/cors.php
@@ -7,11 +7,13 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-        'https://cinelog-frontend.azurestaticapps.net',
         'http://localhost:5173',
+        'http://127.0.0.1:5173',
     ],
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => [
+        '~^https://.*\.azurestaticapps\.net$~',
+    ],
 
     'allowed_headers' => ['*'],
 
